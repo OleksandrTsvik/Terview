5dfcb55e66c67767d26786c10e3e6e5fd406c183
Oleksandr Tsvik <oleksandr.zwick@gmail.com>
2024-12-06 18:59:38 +0200

    feat(web): add home page template

--- Web/src/app/app.component.ts
+++ Web/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NzLayoutModule, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'terview';
-}
+export class AppComponent {}
