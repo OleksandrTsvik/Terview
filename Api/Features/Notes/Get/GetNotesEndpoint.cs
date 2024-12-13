a47926710291f96403fd8a228c913fb2ef949f45
Oleksandr Tsvik <oleksandr.zwick@gmail.com>
2024-12-13 15:10:59 +0200

    feat(api): get notes tags

--- Api/Features/Notes/Get/GetNotesEndpoint.cs
+++ Api/Features/Notes/Get/GetNotesEndpoint.cs
@@ -17,12 +17,20 @@ public class GetNotesEndpoint : IEndpoint
     }
 
     public static async Task<Ok<PagedList<NoteResponse>>> Handler(
-        [FromQuery] int? pageNumber,
-        [FromQuery] int? pageSize,
+        [FromQuery(Name = "q")] string? query,
+        [FromQuery(Name = "t")] string[]? tags,
+        [FromQuery(Name = "p")] int? pageNumber,
+        [FromQuery(Name = "ps")] int? pageSize,
         IMongoCollection<Note> notesCollection,
         CancellationToken cancellationToken)
     {
         PagedList<NoteResponse> notes = await notesCollection.AsQueryable()
+            .WhereIf(
+                !string.IsNullOrWhiteSpace(query),
+                note => note.Title.Contains(query!) || note.Content.Contains(query!))
+            .WhereIf(
+                tags?.Length > 0,
+                note => tags!.All(tag => note.Tags.Contains(tag)))
             .Select(note => new NoteResponse
             {
                 Id = note.Id,
