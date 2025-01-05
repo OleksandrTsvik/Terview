66de03b03c1669d60ed60ec4ec8767fca5a05aa6
Oleksandr Tsvik <oleksandr.zwick@gmail.com>
2025-01-05 22:57:36 +0200

    feat: delete outbox message

--- Web/src/features/outbox-page/outbox.status.tsx
+++ Web/src/features/outbox-page/outbox.status.tsx
@@ -9,12 +9,12 @@ interface Props {
 
 export default function OutboxStatus({ processedOnUtc, error }: Props) {
   if (!processedOnUtc) {
-    return <span className={classnames([styles.outbox__status, styles.outbox__status_new])}>new</span>;
+    return <span className={classnames([styles.outbox__status, styles.outbox__status_new])}>New</span>;
   }
 
   if (error) {
-    return <span className={classnames([styles.outbox__status, styles.outbox__status_failed])}>failed</span>;
+    return <span className={classnames([styles.outbox__status, styles.outbox__status_failed])}>Failed</span>;
   }
 
-  return <span className={classnames([styles.outbox__status, styles.outbox__status_success])}>success</span>;
+  return <span className={classnames([styles.outbox__status, styles.outbox__status_success])}>Success</span>;
 }
--- Web/src/features/outbox-page/use-outbox-actions.tsx
+++ Web/src/features/outbox-page/use-outbox-actions.tsx
@@ -2,18 +2,32 @@ import { green } from '@ant-design/colors';
 import { CaretRightOutlined } from '@ant-design/icons';
 import { App, MenuProps } from 'antd';
 
-import { useRunEventMutation } from './outbox.api';
+import { DeleteIcon } from '@/components/icons';
+
+import { useDeleteOutboxMessageMutation, useRunEventMutation } from './outbox.api';
 import { OutboxResponse } from './outbox.models';
 
 export default function useOutboxActions() {
-  const { notification } = App.useApp();
+  const { modal, notification } = App.useApp();
 
   const [runEvent] = useRunEventMutation();
+  const [deleteOutboxMessage] = useDeleteOutboxMessageMutation();
 
   const handleRunClick = (outboxMessageId: string) => {
     runEvent({ id: outboxMessageId })
       .unwrap()
       .catch(() => notification.error({ message: 'Виникла помилка' }));
+  };
+
+  const handleDeleteClick = (outboxMessageId: string) => {
+    modal.confirm({
+      content: 'Підтвердіть видалення',
+      okButtonProps: { danger: true },
+      onOk: () =>
+        deleteOutboxMessage({ id: outboxMessageId })
+          .unwrap()
+          .catch(() => notification.error({ message: 'Виникла помилка' })),
+    });
   };
 
   const getOutboxActions = (outboxMessage: OutboxResponse): MenuProps['items'] => [
@@ -22,6 +36,12 @@ export default function useOutboxActions() {
       icon: <CaretRightOutlined style={{ color: green.primary }} />,
       label: 'Запустити',
       onClick: () => handleRunClick(outboxMessage.id),
+    },
+    {
+      key: 'delete',
+      icon: <DeleteIcon />,
+      label: 'Видалити',
+      onClick: () => handleDeleteClick(outboxMessage.id),
     },
   ];
 
