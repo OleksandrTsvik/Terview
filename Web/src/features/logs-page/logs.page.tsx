1daf332c6aba612603b07c4e829ff3ef3421eecb
Oleksandr Tsvik <oleksandr.zwick@gmail.com>
2025-04-15 16:26:01 +0300

    feat(web): add switch for auto refetch logs

--- Web/src/features/logs-page/logs.page.tsx
+++ Web/src/features/logs-page/logs.page.tsx
@@ -1,16 +1,19 @@
-import { Empty, Skeleton, Spin, Typography } from 'antd';
+import { Empty, Skeleton, Spin } from 'antd';
+import { useState } from 'react';
 import { useSearchParams } from 'react-router';
 
-import { stringToNumber } from '@/common/type-converters.utils';
-import RefetchButton from '@/components/refetch-button';
+import { stringToBoolean, stringToNumber } from '@/common/type-converters.utils';
 
 import { useGetLogsQuery } from './logs.api';
 import { QUERY_PARAMS } from './logs.constants';
 import LogsLevels from './logs.levels';
 import LogsList from './logs.list';
 import LogsPagination from './logs.pagination';
+import LogsRefetch from './logs.refetch';
 
-import styles from './logs.module.scss';
+function initStateAutoRefetch() {
+  return stringToBoolean(localStorage.getItem('logs-auto-refetch'));
+}
 
 export default function LogsPage() {
   const [searchParams] = useSearchParams();
@@ -19,13 +22,22 @@ export default function LogsPage() {
   const pageNumber = stringToNumber(searchParams.get(QUERY_PARAMS.PAGE_NUMBER), 1);
   const pageSize = stringToNumber(searchParams.get(QUERY_PARAMS.PAGE_SIZE), 20);
 
+  const [autoRefetch, setAutoRefetch] = useState(initStateAutoRefetch);
+
   const { data, isLoading, isFetching, refetch } = useGetLogsQuery(
     { levels, pageNumber, pageSize },
-    {
-      pollingInterval: 15000,
-      refetchOnMountOrArgChange: true,
-    },
+    autoRefetch
+      ? {
+          pollingInterval: 15000,
+          refetchOnMountOrArgChange: true,
+        }
+      : {},
   );
+
+  const handleAutoRefetchChange = (checked: boolean) => {
+    localStorage.setItem('logs-auto-refetch', checked.toString());
+    setAutoRefetch(checked);
+  };
 
   if (isLoading) {
     return <Skeleton active />;
@@ -37,10 +49,13 @@ export default function LogsPage() {
 
   return (
     <>
-      <Typography.Paragraph className={styles.total} type="secondary">
-        <span>Усього логів: {data.totalItems}</span>
-        <RefetchButton loading={isFetching} onClick={refetch} />
-      </Typography.Paragraph>
+      <LogsRefetch
+        isFetching={isFetching}
+        autoRefetch={autoRefetch}
+        totalItems={data.totalItems}
+        refetch={refetch}
+        onAutoRefetchChange={handleAutoRefetchChange}
+      />
       <Spin spinning={isFetching}>
         <LogsLevels levels={data.logLevels} />
         <LogsList logs={data.items} />
